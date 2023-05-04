a739d65cc73b6f51f57b94c19cd1bcf791af6f8d
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-05-04 18:41:29 +0530

    Update sol-4-positive-negative.cpp

--- sol-4-positive-negative.cpp
+++ sol-4-positive-negative.cpp
@@ -1,3 +1,7 @@
+//Problem-Link: https://www.interviewbit.com/problems/positive-negative/
+//Solved-Date: 04-May-2023
+//Problem-Level: Very Easy
+
 vector<int> Solution::solve(vector<int> &A) {
     int positive = 0, negative = 0;
     for (int i = 0; i < A.size(); i++) {
