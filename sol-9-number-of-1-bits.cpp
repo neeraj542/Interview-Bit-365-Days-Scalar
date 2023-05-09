33b608b632382da8793cd3d4e1c7c807f1b35dc6
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-05-09 22:23:05 +0530

    Update sol-9-number-of-1-bits.cpp

--- sol-9-number-of-1-bits.cpp
+++ sol-9-number-of-1-bits.cpp
@@ -1,3 +1,7 @@
+//Problem-Link: https://www.interviewbit.com/problems/number-of-1-bits/
+//Solved-Date: 09-May-2023
+//Problem-Level: Easy 
+
 int Solution::numSetBits(unsigned int A) {
     int cnt = 0;
     while(A != 0){
