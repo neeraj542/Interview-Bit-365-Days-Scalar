7934f31f4254400f1368cc61961f2eb2483c4f11
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-05-11 22:37:35 +0530

    Update sol-11-max-min.cpp

--- sol-11-max-min.cpp
+++ sol-11-max-min.cpp
@@ -1,3 +1,7 @@
+//Problem-Link: https://www.interviewbit.com/problems/max-min-05542f2f-69aa-4253-9cc7-84eb7bf739c4/
+//Solved-Date: 11-May-2023
+//Problem-Level: Easy 
+
 int Solution::solve(vector<int> &A) {
     int ans;
     return *max_element(A.begin(),A.end()) + *min_element(A.begin(),A.end());
