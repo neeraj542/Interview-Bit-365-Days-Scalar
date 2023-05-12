bcd5e81319a4b4873e9746307836b44f0ac5572e
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-05-12 21:59:35 +0530

    Update sol-12-intersection-of-sorted-arrays.cpp

--- sol-12-intersection-of-sorted-arrays.cpp
+++ sol-12-intersection-of-sorted-arrays.cpp
@@ -1,3 +1,7 @@
+//Problem-Link: https://www.interviewbit.com/problems/intersection-of-sorted-arrays/
+//Solved-Date: 12-May-2023
+//Problem-Level: Easy
+
 vector<int> Solution::intersect(const vector<int> &A, const vector<int> &B) {
     vector<int> ans;
     int i = 0, j = 0;
