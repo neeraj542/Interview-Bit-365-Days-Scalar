ee214cca18e1656b4f147c68409bb27e6ed6befe
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-05-13 23:31:50 +0530

    Create sol-13-first-repeating-element.cpp

---
+++ sol-13-first-repeating-element.cpp
@@ -0,0 +1,14 @@
+//Problem-Link: https://www.interviewbit.com/problems/first-repeating-element/
+//Solved-Date: 13-May-2023
+//Problem-Level: Easy
+
+int Solution::solve(vector<int> &A) {
+    unordered_map<int,int>map; 
+    
+    for(int i=0;i<A.size();i++) map[A[i]]++; 
+    for(int i=0;i<A.size();i++) 
+        if(map[A[i]] > 1) 
+            return A[i];
+    return -1;
+
+}
