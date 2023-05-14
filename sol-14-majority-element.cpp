7a1229f0fad93eb5507f76ba534c918a92bbf41f
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-05-14 23:40:36 +0530

    Update sol-14-majority-element.cpp

--- sol-14-majority-element.cpp
+++ sol-14-majority-element.cpp
@@ -2,7 +2,7 @@
 //Solved-Date: 14-May-2023 
 //Problem-Level: Easy 
 
-int Solution::majorityElement(const vector<int> &A) { 
+int Solution::majorityElement(const vector<int> &A) {  
     vector<int> B = A; //make a Copy as input vector is const
     std::sort(B.begin(), B.end()); //sort 
     return B[B.size()/2]; // If there exist a number is present more than n/2 times, than definitely it will be the middle number.
